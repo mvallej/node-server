8e4c62094245a90d86298dba0add56638ffcb28d
María Val <mvallej45@gmail.com>
2023-10-20 19:16:02 -0500

    async

--- tareas.js
+++ tareas.js
@@ -1,10 +1,3 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 const tasks = [];
 
 function printTasks() {
@@ -16,47 +9,47 @@ function printTasks() {
 }
 
 function addTask(description) {
-  tasks.push({ description, completed: false });
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      const task = {
+        id: tasks.length + 1,
+        description,
+        completed: false,
+      };
+      tasks.push(task);
+      resolve(`Tarea "${description}" añadida.`);
+    }, 2000);
+  })
 }
 
 function deleteTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+    if (index >= 0 && index < tasks.length) {
+      const task = tasks.splice(index, 1)[0];
+      resolve("Tarea + task.description + eliminada.");
+    } else {
+      reject("error");
+    }}, 1000)
+  })
 }
 
 function completeTask(index) {
+  new Promise((resolve, reject) => {
+    setTimeout(()=>{
   if (index >= 0 && index < tasks.length) {
     tasks[index].completed = true;
+    resolve("Tarea + {tasks[index].description} + marcada como completada.");
+  } else {
+  reject("Índice de tarea no válido.");
   }
+}, 3000)
+})
 }
 
-rl.question('¿Qué acción deseas realizar? (add/delete/complete/exit): ', (action) => {
-  if (action === 'exit') {
-    console.log('Adiós.');
-    rl.close();
-  } else if (action === 'add') {
-    rl.question('Escribe la descripción de la tarea: ', (description) => {
-      addTask(description);
-      printTasks();
-      rl.close();
-    });
-  } else if (action === 'delete') {
-    printTasks();
-    rl.question('Escribe el número de la tarea que deseas eliminar: ', (index) => {
-      deleteTask(index - 1); // Restamos 1 para ajustar al índice del array
-      printTasks();
-      rl.close();
-    });
-  } else if (action === 'complete') {
-    printTasks();
-    rl.question('Escribe el número de la tarea que deseas marcar como completada: ', (index) => {
-      completeTask(index - 1); // Restamos 1 para ajustar al índice del array
-      printTasks();
-      rl.close();
-    });
-  } else {
-    console.log('Acción no válida. Por favor, elige una acción válida.');
-    rl.close();
-  }
-});
+
+addTask(desayunar).then((data)=> console.log(data));
+deleteTask(2).then((data)=> console.log(data));
+
+console.log(addTask())
+
