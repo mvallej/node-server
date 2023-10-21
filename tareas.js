f594ce6646c8b6bcb2fdddb8fa9a55ae3318a186
María Val <mvallej45@gmail.com>
2023-10-20 19:18:40 -0500

    promises y setTimeout

--- tareas.js
+++ tareas.js
@@ -48,8 +48,37 @@ function completeTask(index) {
 }
 
 
-addTask(desayunar).then((data)=> console.log(data));
-deleteTask(2).then((data)=> console.log(data));
+// addTask(desayunar).then((data)=> console.log(data));
+// deleteTask(2).then((data)=> console.log(data));
 
-console.log(addTask())
+// console.log(addTask())
 
+rl.question('¿Qué acción deseas realizar? (add/delete/complete/exit): ', (action) => {
+  if (action === 'exit') {
+    console.log('Adiós.');
+    rl.close();
+  } else if (action === 'add') {
+    rl.question('Escribe la descripción de la tarea: ', (description) => {
+      addTask(description);
+      printTasks();
+      rl.close();
+    });
+  } else if (action === 'delete') {
+    printTasks();
+    rl.question('Escribe el número de la tarea que deseas eliminar: ', (index) => {
+      deleteTask(index - 1); // Restamos 1 para ajustar al índice del array
+      printTasks();
+      rl.close();
+    });
+  } else if (action === 'complete') {
+    printTasks();
+    rl.question('Escribe el número de la tarea que deseas marcar como completada: ', (index) => {
+      completeTask(index - 1); // Restamos 1 para ajustar al índice del array
+      printTasks();
+      rl.close();
+    });
+  } else {
+    console.log('Acción no válida. Por favor, elige una acción válida.');
+    rl.close();
+  }
+});
